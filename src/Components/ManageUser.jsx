6a5ffde09b03144e5585e76de0160634e6ced209
avi3175 <avishahriar@.com>
2023-06-10 02:56:05 +0600

    just made the table

--- src/Components/ManageUser.jsx
+++ src/Components/ManageUser.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
 import userStack from '../hooks/Userstack';
+import { Helmet } from 'react-helmet-async';
 
 const ManageUser = () => {
     const [cart] = userStack()
     return (
         <div>
-            {cart.length}
+            <Helmet><title>MAGIC SCHOOL || ALL USERS</title></Helmet>
+            <h1 className='text-3xl font-bold'>TOTAL USERS:{cart.length}</h1>
+
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>NAME</th>
+                            <th>EMAIL</th>
+                            <th>ROLE</th>
+                            <th>PASSWORD</th>
+                            <th>MAKE ADMIN</th>
+                            <th>MAKE INSTRUCTOR</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       
+                        {
+                            cart.map((user,index)=><tr>
+                                <th>{index+1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.role}</td>
+                                <td>{user.password}</td>
+                                <td>MAKE ME ADMIN</td>
+                                <td>MAKE ME INSTRUCTOR</td>
+                            </tr>)
+                        }
+                       
+                        
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
