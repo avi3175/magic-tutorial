9dad67713d1d11f6aaf572d6be4e9181bb205d3e
avi3175 <avishahriar@.com>
2023-06-10 18:50:13 +0600

    It seems jwt is begining to work

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
 import {app} from "../firebase/firebase.config"
+import axios from 'axios';
 export const AuthContext = createContext(null)
 
 
@@ -40,6 +41,13 @@ const AuthProvider = ({children}) => {
     useEffect(()=>{
       const unsubscribe = onAuthStateChanged(auth,currentUser=>{
             setuser(currentUser)
+            // get and set token
+            if(currentUser){
+                axios.post('http://localhost:5000/jwt',{email:currentUser.email})
+                .then(data=>{
+                    console.log(data)
+                })
+            }
             setLoading(false)
         })
         return ()=>{
