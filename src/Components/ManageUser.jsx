fecd42ae3f018c70682ff7a497c8bebf5507c9cc
avi3175 <avishahriar@.com>
2023-06-10 17:11:29 +0600

    make instructor is working

--- src/Components/ManageUser.jsx
+++ src/Components/ManageUser.jsx
@@ -7,35 +7,53 @@ const ManageUser = () => {
     // const [cart,refetch] = userStack()
 
 
-const {data:users=[],refetch} = useQuery(['users'],async ()=>{
-    const res = await fetch('http://localhost:5000/users')
-    return res.json()
-})
+    const { data: users = [], refetch } = useQuery(['users'], async () => {
+        const res = await fetch('http://localhost:5000/users')
+        return res.json()
+    })
 
 
 
+    const makeAdmin = (user) => {
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: "PATCH"
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount) {
+                    // refetch()
+                    alert('MAKE ADMIN DONE')
+                }
+            })
+    }
 
 
 
-
-    const makeAdmin = (user) =>{
-            fetch(`http://localhost:5000/users/admin/${user._id}`,{
+    const makeInstructor = (user) => {
+            fetch(`http://localhost:5000/users/instructor/${user._id}`,{
                 method:"PATCH"
             })
             .then(res=>res.json())
             .then(data=>{
                 if(data.modifiedCount){
-                    // refetch()
-                    alert('UPDATE DONE')
+                    alert('MAKE INSTRUCTOR DONE')
                 }
             })
     }
+
+
+
+
+
+
+
+
     return (
         <div>
             <Helmet><title>MAGIC SCHOOL || ALL USERS</title></Helmet>
             <h1 className='text-3xl font-bold'>TOTAL USERS:{users.length}</h1>
 
-            <div className="overflow-x-auto">
+            <div className="overflow-x-auto mt-32">
                 <table className="table table-zebra">
                     {/* head */}
                     <thead>
@@ -50,23 +68,23 @@ const {data:users=[],refetch} = useQuery(['users'],async ()=>{
                         </tr>
                     </thead>
                     <tbody>
-                       
+
                         {
-                            users.map((user,index)=><tr
-                            key={user._id}
+                            users.map((user, index) => <tr
+                                key={user._id}
                             >
-                                <th>{index+1}</th>
+                                <th>{index + 1}</th>
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
                                 <td>{user.role}</td>
                                 <td>{user.password}</td>
-                                <td><button onClick={()=>makeAdmin(user)} className='bg-rose-900 p-2 text-white'>MAKE ME ADMIN</button></td>
-                                <td><button className='bg-blue-900 p-2 text-white'>MAKE ME INSTRUCTOR</button></td>
-                                
+                                <td><button onClick={() => makeAdmin(user)} className='bg-rose-900 p-2 text-white'>MAKE ME ADMIN</button></td>
+                                <td><button onClick={()=>makeInstructor(user)} className='bg-blue-900 p-2 text-white'>MAKE ME INSTRUCTOR</button></td>
+
                             </tr>)
                         }
-                       
-                        
+
+
                     </tbody>
                 </table>
             </div>
