37f756581eb6e40e2cb79be42609c55607d2bf77
avi3175 <avishahriar@.com>
2023-06-11 10:25:10 +0600

    jwt is working

--- src/hooks/Userstack.jsx
+++ src/hooks/Userstack.jsx
@@ -1,16 +1,36 @@
 import { useQuery } from '@tanstack/react-query'
 import { useContext } from 'react'
 import { AuthContext } from '../Provider/AuthProvider'
-const userStack = () =>{
-        const {user} = useContext(AuthContext)
-        const { data:cart=[],refetch } = useQuery({
-            queryKey: ['cart',user?.email],
-            queryFn: async ()=>{
-                const response = await fetch(`http://localhost:5000/cart?email=${user.email}`)
-                return response.json()
-            },
-          })
-        return [cart,refetch]
-        
+// import useAxiosSecure from './useAxiosSecure'
+// import useAuth from './useAuth'
+const userStack = () => {
+  const { user } = useContext(AuthContext)
+  console.log(user)
+  const token = localStorage.getItem('access token')
+  // const [axiosSecure] = useAxiosSecure()
+
+  const { data: cart = [], refetch } = useQuery({
+    queryKey: ['cart', user?.email],
+
+    queryFn: async ()=>{
+        const response = await fetch(`http://localhost:5000/cart?email=${user.email}`,{headers:{
+          authorization:`bearer ${token}`
+        }})
+        return response.json()
+    },
+
+
+
+    // queryFn: async () => {
+    //   const res = await axiosSecure(`/cart?email=${user.email}`)
+    //   console.log('res from axios', res)
+    //   return res.data
+    // },
+
+
+
+  })
+  return [cart, refetch]
+
 }
 export default userStack
